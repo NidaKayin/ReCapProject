f061c6f8bd0e054112cb539b123e4809fa2ada1f
NidaKayin <kayinnida@gmail.com>
2022-01-28 13:09:15 +0300

    project structures changed

---
+++ src/main/java/com/etiya/ReCapProject/core/adapters/PaymentService.java
@@ -0,0 +1,8 @@
+package com.etiya.ReCapProject.core.adapters;
+
+import com.etiya.ReCapProject.entities.requests.PosServiceRequest;
+
+public interface PaymentService {
+
+	public boolean withdraw(PosServiceRequest fakePosServiceRequest);
+}
